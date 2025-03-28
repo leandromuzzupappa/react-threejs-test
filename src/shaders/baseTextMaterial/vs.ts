8a4608a211e39a0d06e8680b5b9c24099f8163ab
Leandro Muzzupappa <lnmuzzupappa@gmail.com>
2025-03-28 01:54:44 -0300

    Refactor: update text vs with screen displacement and some varying variables

--- src/shaders/baseTextMaterial/vs.ts
+++ src/shaders/baseTextMaterial/vs.ts
@@ -1,13 +1,31 @@
 export default /* glsl */ `
   varying vec2 vUv;
+  varying vec3 vPosition;
+  varying vec3 vNormal;
+  uniform float uVelocity;
 
-  void main() {
-    // projectionMatrix -> it is a matrix that is used to convert the 3D coordinates to 2D coordinates.
-    // modelViewMatrix -> it is a matrix that is used to convert the 3D coordinates to 2D coordinates.
-    // position -> it is a 3D vector that represents the position of the vertex.
-    // gl_Position -> it is a 4D vector that represents the position of the vertex.
+  vec3 applyScreenRelativeDisplacement(vec3 position, mat4 modelMatrix, mat4 viewMatrix, mat4 projectionMatrix, float uVelocity) {
+    vec3 worldPosition = (modelMatrix * vec4(position, 1.0)).xyz;
+    vec4 clipPosition = projectionMatrix * viewMatrix * vec4(worldPosition, 1.0);
+    vec2 screenPos = clipPosition.xy / clipPosition.w;  
 
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    float strength = 1.0 - abs(screenPos.x);  
+
+    position.y += worldPosition.x * -uVelocity * strength ;
+
+    return position;
+  }
+
+  void main() {	
+    vec3 newPosition = applyScreenRelativeDisplacement(position, modelMatrix, viewMatrix, projectionMatrix, uVelocity);
+
+    vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+    gl_Position = projectedPosition;
+
     vUv = uv;
+    vNormal = (modelMatrix * vec4(normal,0.0)).xyz;
+    vPosition = newPosition;
   }
 `;
