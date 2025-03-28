f4a29eb958ec2453db57f81b637fca9ad8300b51
Leandro Muzzupappa <lnmuzzupappa@gmail.com>
2025-03-28 01:56:00 -0300

    Feat: add opacity and velocity uniforms to base shader

--- src/shaders/baseMaterial/fs.ts
+++ src/shaders/baseMaterial/fs.ts
@@ -1,11 +1,12 @@
 export default /* glsl */ `
   uniform vec3 uColor;
+  uniform float uOpacity;
   varying vec2 vUv;
 
   void main() {
     vec2 uv = vUv;
     vec3 color = uColor;
-    float opacity = 1.;
+    float opacity = uOpacity;
 
     gl_FragColor = vec4(color, opacity);
   }
--- src/shaders/baseMaterial/vs.ts
+++ src/shaders/baseMaterial/vs.ts
@@ -1,14 +1,14 @@
 export default /* glsl */ `
-  uniform float uVelocity;
-
   varying vec2 vUv;
   varying vec3 vPosition;
   varying vec3 vNormal;
+  uniform float uVelocity;
 
   vec3 applyScreenRelativeDisplacement(vec3 position, mat4 modelMatrix, mat4 viewMatrix, mat4 projectionMatrix, float uVelocity) {
     vec3 worldPosition = (modelMatrix * vec4(position, 1.0)).xyz;
     vec4 clipPosition = projectionMatrix * viewMatrix * vec4(worldPosition, 1.0);
     vec2 screenPos = clipPosition.xy / clipPosition.w;  
+
     float strength = 1.0 - abs(screenPos.x);  
 
     position.y += position.x * -uVelocity * strength ;
