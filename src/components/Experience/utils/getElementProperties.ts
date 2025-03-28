af539c6e78d744ff2391e817380c61411b862298
Leandro Muzzupappa <lnmuzzupappa@gmail.com>
2025-03-28 01:58:03 -0300

    Feat: add transparency detection to els

--- src/components/Experience/utils/getElementProperties.ts
+++ src/components/Experience/utils/getElementProperties.ts
@@ -1,16 +1,29 @@
+const fontMap = {
+  Montreal1: "/fonts/PPNeueMontreal-Variable.ttf",
+  Montreal:
+    "https://rawcdn.githack.com/google/fonts/3b179b729ac3306ab2a249d848d94ff08b90a0af/apache/robotoslab/static/RobotoSlab-Black.ttf",
+};
+
 export const getElementProperties = (element: HTMLElement) => {
   const { backgroundColor, color, fontFamily, fontSize } =
     getComputedStyle(element);
+
+  const isTransparent = element.dataset.glTransparent === "true";
 
   const normalizedFontFamily = fontFamily
     .replace(/['"]/g, "")
     .split(",")[0]
     .trim();
 
+  console.log("Lenny FF", normalizedFontFamily);
+
   return {
     backgroundColor,
     color,
-    fontFamily: normalizedFontFamily,
+    fontFamily:
+      fontMap[normalizedFontFamily as keyof typeof fontMap] ||
+      normalizedFontFamily,
     fontSize: parseFloat(fontSize),
+    isTransparent,
   };
 };
