1373050fac8f82395ef8e40cefa5420a94f0bf55
Leandro Muzzupappa <lnmuzzupappa@gmail.com>
2025-03-28 01:57:00 -0300

    Feat: add cards to the experience

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useRef } from "react";
+import { useGlobalRefs } from "../../hooks/useGlobalRefs";
+import { GLElementsEnum } from "../Experience";
 import css from "./Card.module.css";
 
 export type CardProps = {
@@ -8,18 +11,44 @@ export type CardProps = {
 };
 
 export const Card = ({ image, heading, url, className }: CardProps) => {
+  const { setRef } = useGlobalRefs();
+
+  const cardRef = useRef<HTMLDivElement>(null);
+  const imageRef = useRef<HTMLImageElement>(null);
+  const headingRef = useRef<HTMLHeadingElement>(null);
+
+  useEffect(() => {
+    if (cardRef.current) setRef(cardRef);
+    if (imageRef.current) setRef(imageRef);
+    if (headingRef.current) setRef(headingRef);
+  }, [setRef]);
+
   return (
     <div
+      ref={cardRef}
       className={`${css.Card} ${className && className}`}
       style={
         {
           "--bg-card": `url(${image})`,
         } as React.CSSProperties
       }
+      data-gl={GLElementsEnum.BLOCK}
     >
-      <img className={css.image} src={image} alt={heading} />
+      <img
+        ref={imageRef}
+        className={css.image}
+        src={image}
+        alt={heading}
+        data-gl={GLElementsEnum.IMAGE}
+      />
       <div className={css.cardContent}>
-        <h2 className={css.heading}>{heading}</h2>
+        <h2
+          ref={headingRef}
+          className={css.heading}
+          data-gl={GLElementsEnum.TEXT}
+        >
+          {heading}
+        </h2>
         {url && (
           <a href={url} className={css.link}>
             Go to {heading}
