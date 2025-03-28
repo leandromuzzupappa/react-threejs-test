455b8ab24ecbb3f59c4bccba65cdaf0a27769b5f
Leandro Muzzupappa <lnmuzzupappa@gmail.com>
2025-03-28 02:03:33 -0300

    .

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,28 +1,5 @@
 import * as THREE from "three";
 
-declare module "troika-three-text" {
-  export class Text {
-    text: string | number;
-    fontSize: number;
-    color: string;
-    anchorX?: string | number;
-    anchorY?: string | number;
-    textAlign: string;
-    lineHeight: number;
-    font: string;
-    visible: boolean;
-    material: THREE.ShaderMaterial;
-
-    sync(): void;
-    dispose(): void;
-    preloadFont(): void;
-  }
-  export function preloadFont(
-    options: { font: string; characters?: string },
-    callback?: () => void
-  ): void;
-}
-
 export type MeshShaderMaterial = THREE.Mesh<
   THREE.BufferGeometry<THREE.NormalBufferAttributes>,
   THREE.ShaderMaterial,
