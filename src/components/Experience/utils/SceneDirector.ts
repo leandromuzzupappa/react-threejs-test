d9653c273f08f79325a9690ca427e4cb43feebce
Leandro Muzzupappa <lnmuzzupappa@gmail.com>
2025-03-28 01:58:56 -0300

    Feat: refactor scene director to support text elements and its positions

--- src/components/Experience/utils/SceneDirector.ts
+++ src/components/Experience/utils/SceneDirector.ts
@@ -4,13 +4,16 @@ import { getElementProperties } from "./getElementProperties";
 
 import { fragmentShader, vertexShader } from "../../../shaders/baseMaterial";
 import { fragmentShader as imageFs } from "../../../shaders/baseImageMaterial";
+import { vertexShader as textVs } from "../../../shaders/baseTextMaterial";
+import { Text } from "troika-three-text";
+import { MeshShaderMaterial } from "../../../types";
 
 export class SceneDirector {
   public hideElements = false;
 
   public allElements: HTMLElement[] = [];
-  public allMeshes: THREE.Mesh[] = [];
-  public allTextMeshes: THREE.Mesh[] = [];
+  public allMeshes: MeshShaderMaterial[] = [];
+  public allTextMeshes: THREE.Group[] = [];
 
   public materialCache: Map<string, THREE.ShaderMaterial> = new Map();
 
@@ -22,18 +25,20 @@ export class SceneDirector {
     uniforms: {
       uVelocity: new THREE.Uniform(0),
       uTime: new THREE.Uniform(0),
+      uOpacity: new THREE.Uniform(1),
+      uRadius: new THREE.Uniform(0),
     },
   });
 
   constructor(elements: HTMLElement[], hideElements = false) {
     this.hideElements = hideElements;
     this.allElements = elements;
+
+    console.log("{{ SceneDirector constructor }}", this.allElements);
   }
 
   public getBlockMaterial = (element: HTMLElement): THREE.ShaderMaterial => {
-    const { backgroundColor } = getElementProperties(element);
-
-    console.log("Lenny background material", backgroundColor);
+    const { backgroundColor, isTransparent } = getElementProperties(element);
 
     let material = this.materialCache.get(`block-${backgroundColor}`);
 
@@ -44,7 +49,9 @@ export class SceneDirector {
         new THREE.Color(backgroundColor).toArray()
       );
 
-      // this.materialCache.set(`block-${backgroundColor}`, clonedMaterial);
+      if (isTransparent)
+        clonedMaterial.uniforms.uOpacity = new THREE.Uniform(0);
+
       this.materialCache.set(`block-${backgroundColor}`, clonedMaterial);
       material = clonedMaterial;
     }
@@ -72,14 +79,13 @@ export class SceneDirector {
   };
 
   public getTextMaterial = (element: HTMLElement): THREE.ShaderMaterial => {
-    // const { color } = getElementProperties(element);
     const { color } = getElementProperties(element);
 
     let material = this.materialCache.get(`text-${color}`);
     if (!material) {
       const clonedMaterial = this.baseMaterial.clone();
       clonedMaterial.fragmentShader = fragmentShader;
-      clonedMaterial.vertexShader = vertexShader;
+      clonedMaterial.vertexShader = textVs;
       clonedMaterial.uniforms.uColor = new THREE.Uniform(
         new THREE.Color(color).toArray()
       );
@@ -91,7 +97,7 @@ export class SceneDirector {
     return material;
   };
 
-  public createMesh = (element: HTMLElement): THREE.Mesh => {
+  public createMesh = (element: HTMLElement): MeshShaderMaterial => {
     const elementType = element.dataset.gl;
     const { width, height } = element.getBoundingClientRect();
 
@@ -101,12 +107,31 @@ export class SceneDirector {
       material = this.getBlockMaterial(element as HTMLDivElement);
     }
 
-    if (elementType === GLElementsEnum.TEXT) {
-      material = this.getTextMaterial(element);
-    }
-
     if (elementType === GLElementsEnum.IMAGE) {
       material = this.getImageMaterial(element as HTMLImageElement);
+    }
+
+    if (elementType === GLElementsEnum.TEXT) {
+      material = this.getTextMaterial(element);
+
+      const textMesh = new Text();
+      const { fontFamily, fontSize } = getElementProperties(element);
+
+      textMesh.text = element.textContent!;
+      textMesh.fontSize = fontSize;
+      textMesh.font = fontFamily;
+      textMesh.anchorX = "center";
+      textMesh.anchorY = "50%";
+      textMesh.textAlign = "left";
+      textMesh.material = material;
+      textMesh.sync();
+
+      const group = new THREE.Group();
+      group.add(textMesh as unknown as THREE.Mesh);
+      group.userData.htmlElement = element;
+
+      this.allTextMeshes.push(group);
+      return group as unknown as MeshShaderMaterial;
     }
 
     const mesh = new THREE.Mesh(this.geometry, material);
@@ -118,18 +143,56 @@ export class SceneDirector {
 
   public init = (): void => {
     this.allElements.forEach((element) => {
-      // [TODO] definir como separar entre texto y bloques o imagenes
-
       const obj = this.createMesh(element);
       this.allMeshes.push(obj);
 
       if (this.hideElements) {
-        element.style.opacity = "0";
+        element.style.opacity = "0.2";
       }
     });
+
+    this.updatePlanesPosition(this.allMeshes);
+    this.updateTexts2DPosition(this.allTextMeshes);
   };
 
   public getMeshes = (): THREE.Mesh[] => {
     return this.allMeshes;
+  };
+
+  public updatePlanesPosition = (
+    planes: MeshShaderMaterial[] | THREE.Group[]
+  ) => {
+    planes.forEach((plane) => {
+      const { left, top, width, height } =
+        plane.userData.htmlElement.getBoundingClientRect();
+
+      plane.position.set(
+        left - document.body.offsetWidth / 2 + width / 2,
+        -top + window.innerHeight / 2 - height / 2,
+        0
+      );
+    });
+  };
+
+  public updateTexts2DPosition = (planes: THREE.Group[]) => {
+    planes.forEach((plane) => {
+      const { left, top } = plane.userData.htmlElement.getBoundingClientRect();
+      plane.position.set(
+        left - document.body.offsetWidth / 2,
+        -top + window.innerHeight / 2,
+        0
+      );
+    });
+  };
+
+  public tick = (elapsedTime: number): void => {
+    this.materialCache.forEach((material) => {
+      if (material.uniforms.uTime) {
+        material.uniforms.uTime.value = elapsedTime;
+      }
+    });
+
+    this.updatePlanesPosition(this.allMeshes);
+    this.updateTexts2DPosition(this.allTextMeshes);
   };
 }
